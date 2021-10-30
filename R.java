0d47c1d29bc7f9034511cb565eb35d31185a9c0f
coderharshit1 <93389273+coderharshit1@users.noreply.github.com>
2021-10-30 20:57:16 +0530

    Add files via upload

---
+++ R.java
@@ -0,0 +1,30 @@
+import java.util.Arrays; 
+import java.util.List; 
+  
+
+class RemoveZero 
+{ 
+    public static String removeZero(String str) 
+    { 
+         
+        int i = 0; 
+        while (str.charAt(i) == '0') 
+            i++; 
+  
+         
+        StringBuffer sb = new StringBuffer(str); 
+  
+         
+        sb.replace(0, i, ""); 
+  
+        return sb.toString();  // return in String 
+    } 
+  
+     
+    public static void main (String[] args) 
+    { 
+        String str = "00000123569"; 
+        str = removeZero(str); 
+        System.out.println(str); 
+    } 
+} 
